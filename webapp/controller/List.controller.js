6b57b5edf5f52bd67b25632f40e5f1bcc61c31ba
jkcomt <jkcomt@gmail.com>
2023-03-04 21:06:03 +0000

    filter,filteroperator,overflowtoolbar

--- webapp/controller/List.controller.js
+++ webapp/controller/List.controller.js
@@ -1,12 +1,14 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,JSONModel,MessageToast) {
+    function (Controller,JSONModel,MessageToast,Filter,FilterOperator) {
         "use strict";
 
         return Controller.extend("appviewcatalog.controller.List", {
@@ -17,6 +19,20 @@ sap.ui.define([
             onPressProduct: function(oEvent){
                 let oItem = oEvent.getSource().getSelectedItem().getBindingContext("mProduct").getObject();
                 MessageToast.show(oItem.Name);
+            },
+
+            onSearch: function(oEvent){
+                let aFilters = [];
+                let sQuery = oEvent.getSource().getValue();
+                
+                if(sQuery && sQuery.length > 0){
+                    let filter = new Filter("Name", FilterOperator.Contains, sQuery);
+                    aFilters.push(filter);
+                }
+
+                let oList = this.byId("listProducts");
+                let oBinding = oList.getBinding("items");
+                oBinding.filter(aFilters)
             }
         });
     });
