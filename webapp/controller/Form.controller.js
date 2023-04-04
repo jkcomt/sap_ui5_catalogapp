902f28aee163b55b85d004f0224fac2bcb0a52a3
jkcomt <jkcomt@gmail.com>
2023-04-04 20:41:20 +0000

    formulario

--- webapp/controller/Form.controller.js
+++ webapp/controller/Form.controller.js
@@ -1,16 +1,40 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/core/UIComponent"
+	"sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,UIComponent) {
+    function (Controller,JSONModel) {
         "use strict";
 
         return Controller.extend("appviewcatalog.controller.Form", {
             onInit: function () {
+                let newProduct = {
+                    "ProductId": "",
+                    "Category": "",
+                    "MainCategory": "",
+                    "TaxTarifCode": "1",
+                    "SupplierName": "",
+                    "WeightMeasure": 4,
+                    "WeightUnit": "KG",
+                    "Description": "",
+                    "Name": "",
+                    "DateOfSale": "",
+                    "ProductPicUrl": "",
+                    "Status": "Available",
+                    "Quantity": 10,
+                    "UoM": "PC",
+                    "CurrencyCode": "EUR",
+                    "Price": 100,
+                    "Width": 30,
+                    "Depth": 18,
+                    "Height": 3,
+                    "DimUnit": "cm"
+                }
 
+                let oNewProduct = new JSONModel(newProduct);
+                this.getView().setModel(oNewProduct, "mNewProduct");
             }
         });
     });
